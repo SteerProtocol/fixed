6d6acde6a20ad5d37f2e74913c39d1ad0eee62c0
JairusSW <jairus.v.tanaka@outlook.com>
2024-03-11 15:32:57 -0700

    getTickFromPrice

--- assembly/fixed64.ts
+++ assembly/fixed64.ts
@@ -60,6 +60,27 @@ export class Fixed64 {
     return new Fixed64(l.num * r.num, l.mag * r.mag);
   }
   /**
+   * Divides divident and divisor to calculate the quotient without accuracy
+   * @param dividend Number | String | Fixed
+   * @param divisor Number | String | Fixed
+   * @param mode 0 = raw | 1 = nearest | 2 = ceil | 3 = floor
+   * @returns Fixed
+   */
+  static divi<D, A>(dividend: D, divisor: A, mode: i32 = 0): Fixed64 {
+    const l = Fixed64.from(dividend);
+    const r = Fixed64.from(divisor);
+    console.log(`LN: ${l.num} LM: ${l.mag} RN: ${r.num} RM: ${r.mag}`)
+    if (l.mag >= r.mag) {
+      const expansion = 100;
+      const result = (l.num * expansion) / r.num;
+      return new Fixed64(result, expansion);
+    } else {
+      const expansion = 100;
+      const result = (l.num * expansion) / r.num;
+      return new Fixed64(result, expansion);
+    }
+  }
+  /**
    * Divides divident and divisor to calculate the quotient.
    * @param dividend Number | String | Fixed
    * @param divisor Number | String | Fixed
--- assembly/test.ts
+++ assembly/test.ts
@@ -1,3 +1,14 @@
 import { Fixed64 } from "./fixed64";
+//                                      9999500033329732
+const c = new Fixed64(99995000333, 10000000000000000);
+console.log(c.toString())
+console.log(Math.log(1.0001).toString())
 
-console.log(Fixed64.log(10).toString())
+const p = 12345;
+function getTickFromPrice(price: f64): f64 {
+    const tick = Math.log(price) / Math.log(f64(1.0001));
+    return tick;
+}
+console.log("Fixed.log " + Fixed64.log(p, 1000000000000000).toString());
+console.log("Math.log  " + Math.log(p).toString());
+console.log(`Real: ${getTickFromPrice(p)} Fixed: ${Fixed64.divi(Fixed64.log(p, 1000000000000000), c)}`)
